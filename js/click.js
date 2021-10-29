0235224c3e8b220eb1062ea5a89f5db595887495
DanielAmbani00 <dahniedahn32@gmail.com>
2021-10-29 15:06:21 +0300

    hover

--- js/click.js
+++ js/click.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
-    $("#btn1").click(function(){
-      $("#btn1").toggle();
+    $("#img1").click(function(){
+      $("#img1").toggle();
       $("#btn1").toggle();
     })
     $("#btn1").click(function(){
       $("btn1").toggle();
-      $("#btn1").toggle();
+      $("#img1").toggle();
+    })
+    $("#img2").click(function(){
+      $("#img2").toggle();
+      $("#btn2").toggle();
     })
     $("#btn2").click(function(){
       $("#btn2").toggle();
-      $("#btn2").toggle();
+      $("#img2").toggle();
     })
-    $("#btn").click(function(){
-      $("#btn2").toggle();
-      $("#btn2").toggle();
-    })
-    $("#btn3").click(function(){
-      $("#btn3").toggle();
+    $("#img3").click(function(){
+      $("#img3").toggle();
       $("#btn3").toggle();
     })
     $("#btn3").click(function(){
       $("#btn3").toggle();
-      $("#btn3").toggle();
+      $("#img3").toggle();
     })
+    
   })
   
   function response(){
     var userName = document.getElementById("name").value;
     alert(userName + " we have recieved your message. Thank you for reaching out to us.");
   }
+  
